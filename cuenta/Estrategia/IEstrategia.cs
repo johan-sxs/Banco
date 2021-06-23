e827cef2971d688b2b5e617cfcafaa732dd6885d
johan-sxs <johan.saicoos@gmail.com>
2021-06-23 15:19:56 -0300

    trabajando en tp2

---
+++ cuenta/Estrategia/IEstrategia.cs
@@ -0,0 +1,12 @@
+﻿using cuenta;
+
+namespace banco.Estrategia
+{
+    public interface IEstrategia
+    {
+        void Debitar(Cliente cliente, double monto);
+        void Acreditar(Cliente cliente, double monto);
+        bool EsApto(Cliente cliente);
+    }
+
+}
--- cuenta/cliente.cs
+++ cuenta/cliente.cs
@@ -1,21 +1,43 @@
-﻿using System;
+﻿
+using banco.Estrategia;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace cuenta
 {
-    class Cliente
+    public class Cliente
     {
+        public IEstrategia estrategia;
         public cuenta Cuenta { get; set; }
         public string Nombre;
         public string Apellido;
         public double Efectivo { get; private set; }
         public double saldo { get; set; }
 
-      =new cuenta();
-        
+        public Cliente()
+        {
+            Cuenta = new cuenta();
+            Efectivo = 0;
+        }
+        public int Cbu => Cuenta.Cbu ;
+
+        public void Acreditar(double monto)
+        {
+            Efectivo = Efectivo + monto;
+            Cuenta.Acreditar(monto * 0.2);
+        }
+        public void Debitar(double monto)
+        {
+            Efectivo = Efectivo - monto;
+            Cuenta.Debitar(monto / 0.2);
+
+        }
+        public bool Tienealmenos(double monto) => saldo >= monto;
+             
+        }
     }
-}
+
 
     
 
